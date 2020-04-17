f37fcf9206e501662e0ed5e21ff7adb038d10201
zokaiG <futlouco7@gmail.com>
2020-04-17 09:56:02 -0400

    Completed Lesson 2 - Writing a Unit Test with Jest

---
+++ unit-testing-vue/src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button v-show="loggedIn">Logout</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loggedIn: false
+    };
+  }
+};
+</script>
+
+<style scoped></style>
